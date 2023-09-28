707c29266d592d50413a2807b01d0b423c6d1a90
georgi.penev <georgi.penev@mentormate.com>
2023-09-28 17:45:59 +0300

    add AsyncFactory base class for test purposes

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,7 @@
 import uuid
 from abc import ABC, abstractmethod
 
+import factory
 from sqlalchemy.dialects.postgresql import UUID
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.types import TypeDecorator, CHAR
@@ -60,3 +61,29 @@ class GUID(TypeDecorator):
             if not isinstance(value, uuid.UUID):
                 value = uuid.UUID(value)
             return value
+
+
+class AsyncFactory(factory.alchemy.SQLAlchemyModelFactory):
+    @classmethod
+    async def _create(cls, model_class, *args, **kwargs):
+        instance = super()._create(model_class, *args, **kwargs)
+        async with cls._meta.sqlalchemy_session as session:
+            await session.commit()
+        return instance
+
+    @classmethod
+    async def create_batch(cls, size, **kwargs):
+        """This async wrapper of create_batch create a batch of instances of the given class,
+            with overridden attrs.
+
+        Args:
+            size (int): the number of instances to create
+
+        Returns:
+            object list: the created instances
+        """
+        return [await cls.create(**kwargs) for _ in range(size)]
+
+    @classmethod
+    def set_session(cls, session: AsyncSession):
+        cls._meta.sqlalchemy_session = session
