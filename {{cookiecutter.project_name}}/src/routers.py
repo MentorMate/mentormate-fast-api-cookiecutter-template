a44c4663af1fae6433f632e714b0fb5f143aa8f0
georgi.penev <georgi.penev@mentormate.com>
2023-10-16 18:15:29 +0300

    add template sceleton

--- {{cookiecutter.project_name}}/src/routers.py
+++ {{cookiecutter.project_name}}/src/routers.py
@@ -1,10 +1,12 @@
-{%- if cookiecutter.add_example_apps == 'True' %}
+{%- if cookiecutter.add_example_apps == True %}
 from src.users.v1.routers import router as users_v1
 from src.auth.jwt.v1.routes import router as jwt_v1
+{% else %}
+# Import your apps' routers here
 {% endif -%}
 
 routers = [
-    {%- if cookiecutter.add_example_apps == 'True' %}
+    {%- if cookiecutter.add_example_apps == True %}
     jwt_v1,
     users_v1
     {% endif -%}
